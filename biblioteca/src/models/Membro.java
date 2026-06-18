99f433ab64d96ed4423b79eeb9a2388fe404f098
artursnog <artursantosnogueira2018@gmail.com>
2026-06-18 14:55:39 -0300

    fix : metodos getters/setters repetidos em membro

--- biblioteca/src/models/Membro.java
+++ biblioteca/src/models/Membro.java
@@ -17,22 +17,6 @@ public class Membro extends Usuario {
         this.telefone = telefone;
         this.email = email;
     }
-    
-    public static Membro criaMembro(String nome, String cpf, String login, String senha, boolean ativo, String endereco, String telefone, String email){
-        return new Membro(nome, cpf, login, senha, ativo, endereco, telefone, email);
-    }
-    
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
 
     public String getEndereco() {
         return endereco;
